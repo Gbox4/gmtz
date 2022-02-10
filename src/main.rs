b0826193ceb29db8b249600c928b8d0bdd1b31d4
Gabe Banks <gabriel.t.banks@gmail.com>
2022-02-10 08:36:05 -1000

    made it print current time

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 // Gabe Banks 1/21/22 <https://gabebanks.net>
 
+extern crate chrono;
+use chrono::{Duration,Utc};
+
 use std::{env, process::exit};
 
 fn print_help() {
-    println!("gmtz - get the GMT offset of a timezone.\nUSAGE: gmtz TIMEZONE");
+    println!("gmtz - get the current time in another timezone.\nUSAGE: gmtz TIMEZONE\nEXAMPLE: gmtz EST");
     exit(1);
 }
 
@@ -15,10 +18,10 @@ fn main() {
 
     let tz = &args[1].to_uppercase();
 
-    const TZ_COUNT: usize = 32;
+    const TZ_COUNT: usize = 28;
     // Data from https://publib.boulder.ibm.com/tividd/td/TWS/SC32-1274-02/en_US/HTML/SRF_mst273.htm
-    const TZ_CODES: [&str; TZ_COUNT] = ["GMT", "UTC", "ECT", "EET", "ART", "EAT", "MET", "NET", "PLT", "IST", "BST", "VST", "CTT", "JST", "ACT", "AET", "SST", "NST", "MIT", "HST", "AST", "PST", "PNT", "MST", "CST", "EST", "IET", "PRT", "CNT", "AGT", "BET", "CAT"];
-    const TZ_OFFSETS: [&str; TZ_COUNT] = ["GMT", "GMT", "GMT+1:00", "GMT+2:00", "GMT+2:00", "GMT+3:00", "GMT+3:30", "GMT+4:00", "GMT+5:00", "GMT+5:30", "GMT+6:00", "GMT+7:00", "GMT+8:00", "GMT+9:00", "GMT+9:30", "GMT+10:00", "GMT+11:00", "GMT+12:00", "GMT-11:00", "GMT-10:00", "GMT-9:00", "GMT-8:00", "GMT-7:00", "GMT-7:00", "GMT-6:00", "GMT-5:00", "GMT-5:00", "GMT-4:00", "GMT-3:30", "GMT-3:00", "GMT-3:00", "GMT-1:00"];
+    const TZ_CODES: [&str; TZ_COUNT] = ["GMT", "UTC", "ECT", "EET", "ART", "EAT", "NET", "PLT", "BST", "VST", "CTT", "JST", "AET", "SST", "NST", "MIT", "HST", "AST", "PST", "PNT", "MST", "CST", "EST", "IET", "PRT", "AGT", "BET", "CAT"];
+    const TZ_OFFSETS: [i64; TZ_COUNT] = [0, 0, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, -11, -10, -9, -8, -7, -7, -6, -5, -5, -4, -3, -3, -1];
 
     let mut i = 0;
     while TZ_CODES[i] != tz {
@@ -29,5 +32,7 @@ fn main() {
         }
     }
 
-    println!("{}", TZ_OFFSETS[i]);
+    let dt = Utc::now() + Duration::hours(TZ_OFFSETS[i]);
+    println!("{}", dt.format("%a %R"));
 }
+
